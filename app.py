f1d1602d0e405c29d6a17df83c7d4c834d55a954
Draklob <jbarreiro.23@gmail.com>
2025-04-22 20:44:46 +0200

    Cambios en la tabla salas y hay que comprobar que se introducen bien ahora en ella. Se cambio tambien el salas.json

--- app.py
+++ app.py
@@ -19,14 +19,51 @@ def get_cines():
     try:
         conexion = conectar_base_datos_cine()
         cursor = conexion.cursor( pymysql.cursors.DictCursor )
+        # Cogemos los datos de los cines de la base de datos
         cursor.execute('SELECT id_cine, nombre, direccion, telefono, precio_base FROM cines')
         cines = cursor.fetchall()
+        # Buscamos el numero de salas que tiene cada cine
+
+
         cursor.close()
         conexion.close()
         return jsonify(cines)
     except pymysql.Error as error:
         return jsonify({'error': str(error)}), 500
 
+@app.route('/cines/<int:id_cine>/peliculas', methods=['GET'])
+#def get_peliculas(id_cine, fecha):
+def get_peliculas(id_cine):
+    try:
+        conexion = conectar_base_datos_cine()
+        cursor = conexion.cursor( pymysql.cursors.DictCursor )
+        print("Buscando peliculas")
+        cursor.execute('''
+            SELECT
+                p.id_pelicula, p.titulo,
+                GROUP_CONCAT(CONCAT(s.numero, ' a las ', TIME_FORMAT(f.fecha_hora,'%%H:%%i')) SEPARATOR '; ') AS horarios
+            FROM
+                cines c
+                INNER JOIN salas s ON c.id_cine = s.id_cine
+                INNER JOIN funciones f ON s.id_sala = f.id_sala
+                INNER JOIN peliculas p ON f.id_pelicula = p.id_pelicula
+            WHERE
+                c.id_cine = %s
+                AND DATE(f.fecha_hora) = %s
+            GROUP BY
+                p.id_pelicula, p.titulo, p.duracion
+            ORDER BY 
+                `p`.`id_pelicula` ASC;
+        ''', (id_cine, '2025-04-14'))
+        peliculas = cursor.fetchall()
+        print(peliculas)
+
+        cursor.close()
+        conexion.close()
+        return jsonify(peliculas)
+
+    except pymysql.Error as error:
+        return jsonify({'error': str(error)}), 500
 @app.route('/')
 def serve_index():
     return render_template('index.html')
